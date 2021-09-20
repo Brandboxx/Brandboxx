254f2abcc65dff7a3249aaf5f3376d57f0631a39
Charles Best <charlesbest8@gmail.com>
2021-09-20 14:19:46 +0100

    add onchange to input

--- src/containers/input.js
+++ src/containers/input.js
@@ -1,10 +1,22 @@
 import { Input } from "../components";
 
-const InputContainer = ({ label, width, placeHolder, value, ...rest }) => {
+const InputContainer = ({
+  label,
+  width,
+  placeHolder,
+  onChange,
+  value,
+  ...rest
+}) => {
   return (
     <Input {...rest}>
       <Input.Label>{label}</Input.Label>
-      <Input.TextField width={width} placeholder={placeHolder} value={value} />
+      <Input.TextField
+        width={width}
+        placeholder={placeHolder}
+        onChange={onChange}
+        value={value}
+      />
     </Input>
   );
 };
