c45d7f4392e10ca370c49e4cc13e5f4edd7a1fe4
Charles Best <charlesbest8@gmail.com>
2021-09-23 10:57:41 +0100

    fix store

--- src/reduxSetup/store.js
+++ src/reduxSetup/store.js
@@ -2,16 +2,16 @@ import { createStore, applyMiddleware } from "redux";
 import { getStorage, setStorage } from "../utils/storage";
 import { CENTER_POCKET } from "./constant";
 import RootReducer from "./reducers/rootReducer";
-
-const Store = ()=>{
-    var initialState = getStorage(CENTER_POCKET);
-    const StoreData = (store) => (next) => (action)=>{
-        let result = next(action);
-        const saveData = store.getState();
-        delete saveData.app;
-        setStorage(CENTER_POCKET,saveData);
-        return result
-    }
-    return createStore(RootReducer, initialState, applyMiddleware(StoreData));
-}
+import { initialState as appIntialState } from "./reducers/appReducer";
+const Store = () => {
+  var initialState = getStorage(CENTER_POCKET);
+  const StoreData = (store) => (next) => (action) => {
+    let result = next(action);
+    const saveData = store.getState();
+    saveData.app = appIntialState;
+    setStorage(CENTER_POCKET, saveData);
+    return result;
+  };
+  return createStore(RootReducer, initialState, applyMiddleware(StoreData));
+};
 export default Store();
