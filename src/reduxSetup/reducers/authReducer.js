3e08359af17311ac21fdec866619aa5c9fd4841f
Charles Best <charlesbest8@gmail.com>
2021-09-23 11:06:53 +0100

    add token to auth reducer

--- src/reduxSetup/reducers/authReducer.js
+++ src/reduxSetup/reducers/authReducer.js
@@ -1,14 +1,16 @@
-import { IS_AUTHENTICATED, USER_DETAILS } from "../constant";
+import { IS_AUTHENTICATED, USER_DETAILS, TOKEN } from "../constant";
 
-const initialState = { isAuthenticated: false, userDetails: null }
+const initialState = { isAuthenticated: false, userDetails: null, token:'' };
 
 export const authReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case IS_AUTHENTICATED:
-            return { ...state, isAuthenticated: action.payload }
-        case USER_DETAILS:
-            return { ...state, userDetails: action.payload }
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case IS_AUTHENTICATED:
+      return { ...state, isAuthenticated: action.payload };
+    case USER_DETAILS:
+      return { ...state, userDetails: action.payload };
+    case TOKEN:
+      return { ...state, token: action.payload };
+    default:
+      return state;
+  }
+};
