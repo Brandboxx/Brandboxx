6acb82c1492b3f66ccdf96f2a2647d5695334738
Charles Best <charlesbest8@gmail.com>
2021-09-23 12:07:04 +0100

    add time name util

--- src/utils/dateUtils.js
+++ src/utils/dateUtils.js
@@ -1,34 +1,42 @@
 const months = [
-  'JAN',
-  'FEB',
-  'MAR',
-  'APR',
-  'MAY',
-  'JUN',
-  'JUL',
-  'AUG',
-  'SEPT',
-  'OCT',
-  'NOV',
-  'DEC',
+  "JAN",
+  "FEB",
+  "MAR",
+  "APR",
+  "MAY",
+  "JUN",
+  "JUL",
+  "AUG",
+  "SEPT",
+  "OCT",
+  "NOV",
+  "DEC",
 ];
 function formatAMPM(date) {
-    var hours = date.getHours();
-    var minutes = date.getMinutes();
-    var ampm = hours >= 12 ? 'pm' : 'am';
-    hours = hours % 12;
-    hours = hours ? hours : 12; // the hour '0' should be '12'
-    minutes = minutes < 10 ? '0'+minutes : minutes;
-    var strTime = hours + ':' + minutes + ' ' + ampm;
-    return strTime;
-  }
-  
-export const getDate = dateString => {
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var ampm = hours >= 12 ? "pm" : "am";
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  var strTime = hours + ":" + minutes + " " + ampm;
+  return strTime;
+}
+
+export const getDate = (dateString) => {
   const date = new Date(dateString);
   return `${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`;
 };
 
-export const getDateTime = (dataString)=> {
-    const date = new Date(dataString);
-    return `${date.toDateString()}   ${formatAMPM(date)}`;
-  };
+export const getDateTime = (dataString) => {
+  const date = new Date(dataString);
+  return `${date.toDateString()}   ${formatAMPM(date)}`;
+};
+
+export const timeName = (date = new Date()) => {
+  const hrs = date.getHours();
+  if (hrs < 12) return "morning";
+  else if (hrs >= 12 && hrs <= 17) return "afternoon";
+  else if (hrs >= 17 && hrs <= 24) return "evening";
+  else return 'day'
+};
