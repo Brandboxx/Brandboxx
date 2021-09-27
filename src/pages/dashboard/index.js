5b2cb07a868fdec5885ffee9222b3df0d76085ba
Charles Best <charlesbest8@gmail.com>
2021-09-27 01:14:00 +0100

    complete dashboard

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -12,6 +12,7 @@ import { MainLayout } from "../../pages";
 import { DashModalContainer } from "../../containers";
 import { Info, CardContainer, CardInfo } from "./style";
 import { useGetResquest } from "../../api/useRequestProcessor";
+import { currencyFormatter } from "../../utils/numberFormater";
 
 const Dashboard = () => {
   const [modal, setModal] = useState(false);
@@ -20,6 +21,12 @@ const Dashboard = () => {
     "/users/view-pocket-balance",
     ["users", "view-pocket-balance"]
   );
+
+  const { data: flexTransactions } = useGetResquest(
+    "/deposit/view-recent-transactions?plan_type=flex pocket&plan_code=01",
+    "flexTransactions"
+  );
+
   const [successModal, setSuccessModal] = useState(false);
 
   return (
@@ -37,26 +44,33 @@ const Dashboard = () => {
         <Header setModal={setModal} />
         <Info>
           <PriceDetailHeader
-            amount={`₦${viewPocketBalance?.data?.total ?? "N/A"}`}
+            amount={currencyFormatter(viewPocketBalance?.data?.total) ?? "N/A"}
           />
           <CardContainer>
             <DashCard
               title={"Flex Balance"}
-              amount={`₦${viewPocketBalance?.data?.flexPocket ?? "N/A"}`}
+              amount={
+                currencyFormatter(viewPocketBalance?.data?.flexPocket) ?? "N/A"
+              }
               img={"/assets/svg/dashCards/dash1.svg"}
               bg={"#E7F5F5"}
               cl={"#149A9B"}
             />
             <DashCard
               title={"Pocket Lock Balance"}
-              amount={`₦${viewPocketBalance?.data?.lockPocket ?? "N/A"}`}
+              amount={
+                currencyFormatter(viewPocketBalance?.data?.lockPocket) ?? "N/A"
+              }
               img={"/assets/svg/dashCards/dash2.svg"}
               bg={"#fb70063a"}
               cl={"#FB7106"}
             />
             <DashCard
               title={"Pocket Target Balance"}
-              amount={`₦${viewPocketBalance?.data?.targetPocket ?? "N/A"}`}
+              amount={
+                currencyFormatter(viewPocketBalance?.data?.targetPocket) ??
+                "N/A"
+              }
               img={"/assets/svg/dashCards/dash3.svg"}
               bg={"#59027331"}
               cl={"#580273"}
@@ -69,20 +83,28 @@ const Dashboard = () => {
 
           <CardInfo>
             <h3>Recent Transactions</h3>
-            <div>
-              <main>
-                <img src={"/assets/svg/dashCards/up.svg"} alt={""} />
-                <p>Pocket Flex credited</p>
-              </main>
-              <p>N20,000</p>
-            </div>
-            <div>
-              <main>
-                <img src={"/assets/svg/dashCards/down.svg"} alt={""} />
-                <p>Pocket Flex debited</p>
-              </main>
-              <p>N20,000</p>
-            </div>
+            {flexTransactions?.data.map((transaction, index) => {
+              return (
+                <div>
+                  <main>
+                    {transaction.action === "deposit" ? (
+                      <img src={"/assets/svg/dashCards/up.svg"} alt={""} />
+                    ) : (
+                      <img src={"/assets/svg/dashCards/down.svg"} alt={""} />
+                    )}
+
+                    <p>
+                      Pocket Flex{" "}
+                      {transaction.action === "deposit"
+                        ? " credited"
+                        : " debited"}
+                    </p>
+                  </main>
+                  <p>{currencyFormatter(transaction.amount) ?? "N?A"}</p>
+                </div>
+              );
+            })}
+            {!flexTransactions?.data.length && <>No recent transaction yet</>}
           </CardInfo>
         </Info>
       </MainLayout>
