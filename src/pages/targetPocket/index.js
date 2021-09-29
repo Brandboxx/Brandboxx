dc201dda761178ec452ea2b3d5c40806f8f8d7a5
kelah <kelechiekebor@gmail.com>
2021-09-29 22:02:07 +0100

    current  and completed api integretion

--- src/pages/targetPocket/index.js
+++ src/pages/targetPocket/index.js
@@ -15,14 +15,44 @@ import { POCKETPLANS, TARGETSAVE } from "../../constants/routes";
 import { useHistory } from "react-router-dom";
 
 import { currencyFormatter } from "../../utils/numberFormater";
+import { useState, useEffect } from "react";
 
 
 const TargetPocket = () => {
   const history = useHistory();
+  const [selectedTab, setSelectedTab] = useState("")
   const { data: viewPocketBalance } = useGetResquest(
     "/users/view-pocket-balance",
     ["users", "view-pocket-balance"]
   );
+  
+  const { data: completedTargetPockets } = useGetResquest(
+    "/target-pocket/completed-target-pockets",
+    ["target-pocket", "completed-target-pockets"],
+    selectedTab === 1
+  );
+  const { data: currentTargetPockets } = useGetResquest(
+    "/target-pocket/current-target-pockets",
+    ["target-pocket", "current-target-pockets"],
+    selectedTab === 1
+  );
+
+  const [targetPocketHistory, setTargetPocketHistory] = useState("")
+
+  const Current = selectedTab ? Tab : ActiveTab;
+  const Completed = !selectedTab ? Tab : ActiveTab;
+
+  useEffect(() => {
+    selectedTab === 0
+      ? setTargetPocketHistory(completedTargetPockets)
+      : setTargetPocketHistory(currentTargetPockets);
+      console.log("target pocket",targetPocketHistory?.data)
+  }, 
+
+  [currentTargetPockets, completedTargetPockets, selectedTab]
+  
+  );
+
   return (
     <MainLayout>
       <Container>
@@ -84,14 +114,41 @@ const TargetPocket = () => {
         </CardsContainer>
         <TransactionContainer>
           <TabContainer>
-            <Tab>
+            {/* <Tab>
               <p style={{ color: "rgba(173, 174, 175, 1)" }}>Current</p>
             </Tab>
             <ActiveTab style={{ background: "rgba(88, 2, 115, 1)" }}>
               <p>Completed</p>
-            </ActiveTab>
+            </ActiveTab> */}
+            <Current onClick={() => setSelectedTab(0)}>
+              <p>Current</p>
+            </Current>
+            <Completed onClick={() => setSelectedTab(1)}>
+              <p>Completed</p>
+            </Completed>
           </TabContainer>
-          <Credit>
+
+          {targetPocketHistory?.data?.map((element,index) => {
+            return(
+              <Credit> 
+                <span style={{ display: "flex", alignItems: "center" }}>
+                {" "}
+                <img src={"/assets/svg/circleTarget.svg"} alt={""} />
+                <p style={{ marginLeft: "10px" }}>{element.plan_type}</p>
+              </span>
+              <p>Target: N{element.amount}</p>
+              <p>
+                <span style={{ fontSize: "12px", color: "rgba(88, 2, 115, 1)" }}>
+                  Balance
+                </span>{" "}
+                {element.balance}
+              </p>
+              </Credit>
+            )
+          }
+          )}
+
+          {/* <Credit>
             <span style={{ display: "flex", alignItems: "center" }}>
               {" "}
               <img src={"/assets/svg/circleTarget.svg"} alt={""} />
@@ -132,7 +189,7 @@ const TargetPocket = () => {
               </span>{" "}
               N500
             </p>
-          </Credit>
+          </Credit> */}
         </TransactionContainer>
       </Container>
     </MainLayout>
